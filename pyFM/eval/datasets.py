7abfd4d6bf06d87b51e1b065bfd0cf37c1040c45
Niklas Lüdtke <niklas.luedtke@web.de>
2022-05-24 18:24:36 +0200

    added cp2p dataloader

--- pyFM/eval/datasets.py
+++ pyFM/eval/datasets.py
@@ -1,10 +1,21 @@
-import spectral.projection_utils as pju
-import mesh.trimesh as tm
+import os
+from pathlib import Path
+
+import numpy as np
+import potpourri3d as pp3d
+import torch
+from torch.utils.data import Dataset
+from ..diffusion_net import geometry as dng
+from ..diffusion_net import utils as dnu
+from .utils import farthest_point_sample, square_distance
+
+from ..spectral import projection_utils as pju
+from ..mesh import trimesh as tm
 
 
-
-class Faust():
-
+class Faust:
+    # Calculating p2p ground truth maps for evaluation takes too long!
+    # Use other methods
     def __init__(self, path):
 
         #TODO: create check if its a pathlib.Path instance, otherwise create one
@@ -17,9 +28,9 @@ class Faust():
         type = "scans" if scan else "registrations"
         type_prefix = "scan_" if scan else "reg_"
 
-        paths = [self.path / type / mode / mode_prefix + type_prefix + nr + ".ply" for nr in sample_nr]
+        paths = [self.path / mode / type / (mode_prefix + type_prefix + nr + ".ply") for nr in sample_nr]
 
-        meshes = [tm.TriMesh(path) for path in paths]
+        meshes = [tm.TriMesh(str(path)) for path in paths]
 
         return meshes
 
@@ -33,18 +44,233 @@ class Faust():
 
         Outputs
         -------------------------------
+    
         """
 
         # Load the two representation meshes
         rep_mesh1, rep_mesh2 = self.get_samples([shape1, shape2], test=False, scan=False)
 
-        # Load the scan mesh
-        scan_mesh = self.get_samples([shape1], test=False, scan=True)
+        print("got representation meshes")
+        # Load the scan meshes
+        # scan_mesh1, scan_mesh2 = self.get_samples([shape1, shape2], test=False, scan=True)
+        scan_mesh1 = self.get_samples([shape1], test=False, scan=True)[0]
+        print("Got first scan mesh")
+        rep_mesh2_points = pju.project_point_to_rep_to_rep(rep_mesh1, rep_mesh2, scan_mesh1.vertlist)
+        print("calculated first projection")
+        del scan_mesh1
+        scan_mesh2 = self.get_samples([shape2], test=False, scan=True)[0]
+
+        b, tid = pju.project_pc_to_triangles(scan_mesh2.vertlist, scan_mesh2.facelist, rep_mesh2_points, return_vec=True)
         
-        points = pju.project_point_to_rep_to_rep(rep_mesh1, rep_mesh2, scan_mesh.vertlist)
+        scan2_points = scan_mesh2.barycentric_to_points(b, tid)
 
-        return points
+        del scan_mesh2
 
-    def get_geodesic_error():
+        return scan2_points
+
+    def get_geodesic_error(self):
 
         pass
+
+
+class ShrecPartialDataset(Dataset):
+    def __init__(self, root_dir, name="cuts", k_eig=128, n_fmap=30, use_cache=True, op_cache_dir=None):
+
+        self.k_eig = k_eig
+        self.n_fmap = n_fmap
+        self.root_dir = root_dir
+        self.cache_dir = root_dir
+        self.op_cache_dir = op_cache_dir
+
+        # check the cache
+        if use_cache:
+            load_cache = os.path.join(self.cache_dir, f"cache_{name}_train.pt")
+            print("using dataset cache path: " + str(load_cache))
+            if os.path.exists(load_cache):
+                print("  --> loading dataset from cache")
+                (
+                    self.verts_list,
+                    self.faces_list,
+                    self.frames_list,
+                    self.massvec_list,
+                    self.L_list,
+                    self.evals_list,
+                    self.evecs_list,
+                    self.gradX_list,
+                    self.gradY_list,
+                    self.used_shapes,
+                    self.corres_dict,
+                    self.sample_list,
+                ) = torch.load(load_cache)
+                self.combinations = list(self.corres_dict.keys())
+                return
+            print("  --> dataset not in cache, repopulating")
+
+        # Load the meshes & labels
+        # define files and order
+        self.used_shapes = sorted([x.stem for x in (Path(root_dir) / "shapes").iterdir() if name in x.stem])
+        corres_path = Path(root_dir) / "maps"
+        all_combs = [x.stem for x in corres_path.iterdir() if name in x.stem]
+        self.corres_dict = {}
+        for x, y in map(lambda x: (x[:x.rfind("_")], x[x.rfind("_") + 1:]), all_combs):
+            if x in self.used_shapes and y in self.used_shapes:
+                map_ = torch.from_numpy(np.loadtxt(corres_path / f"{x}_{y}.map", dtype=np.int32)).long()
+                self.corres_dict[(self.used_shapes.index(y), self.used_shapes.index(x))] = map_
+
+        # set combinations
+        self.combinations = list(self.corres_dict.keys())
+        mesh_dirpath = Path(root_dir) / "shapes"
+
+        # Get all the files
+        self.verts_list = []
+        self.faces_list = []
+        self.sample_list = []
+
+        # Load the actual files
+        for shape_name in self.used_shapes:
+            print("loading mesh " + str(shape_name))
+
+            verts, faces = pp3d.read_mesh(str(mesh_dirpath / f"{shape_name}.off"))
+
+            # to torch
+            verts = torch.tensor(np.ascontiguousarray(verts)).float()
+            faces = torch.tensor(np.ascontiguousarray(faces))
+            self.verts_list.append(verts)
+            self.faces_list.append(faces)
+            idx0 = farthest_point_sample(verts.t(), ratio=0.9)
+            dists, idx1 = square_distance(verts.unsqueeze(0), verts[idx0].unsqueeze(0)).sort(dim=-1)
+            dists, idx1 = dists[:, :, :130].clone(), idx1[:, :, :130].clone()
+            self.sample_list.append((idx0, idx1, dists))
+        # Precompute operators
+        (
+            self.frames_list,
+            self.massvec_list,
+            self.L_list,
+            self.evals_list,
+            self.evecs_list,
+            self.gradX_list,
+            self.gradY_list,
+        ) = dng.get_all_operators(
+            self.verts_list,
+            self.faces_list,
+            k_eig=self.k_eig,
+            op_cache_dir=self.op_cache_dir,
+        )
+
+        # save to cache
+        if use_cache:
+            dnu.ensure_dir_exists(self.cache_dir)
+            torch.save(
+                (
+                    self.verts_list,
+                    self.faces_list,
+                    self.frames_list,
+                    self.massvec_list,
+                    self.L_list,
+                    self.evals_list,
+                    self.evecs_list,
+                    self.gradX_list,
+                    self.gradY_list,
+                    self.used_shapes,
+                    self.corres_dict,
+                    self.sample_list,
+                ),
+                load_cache,
+            )
+
+        print("Initialization done!")
+
+    def load_trimesh(self, idx):
+        """
+        Load TriMesh class for given ID's.
+
+        Parameters
+        -------------------------------
+        ids  : list list of ID's of the corresponding Shrec models
+
+        Outputs
+        -------------------------------
+    
+        """
+        idx1, idx2 = self.combinations[idx]
+        ids = [idx1, idx2]
+        meshes = [tm.TriMesh(self.verts_list[idx], self.faces_list[idx]) for idx in ids]
+
+        return meshes
+
+    def get_p2p(self, idx):
+        idx1, idx2 = self.combinations[idx]
+        self.corres_dict[(idx1, idx2)]
+        print(self.corres_dict.keys())
+
+    def __len__(self):
+        return len(self.combinations)
+
+    def __getitem__(self, idx):
+        idx1, idx2 = self.combinations[idx]
+
+        shape1 = {
+            "xyz": self.verts_list[idx1],
+            "faces": self.faces_list[idx1],
+            "frames": self.frames_list[idx1],
+            "mass": self.massvec_list[idx1],
+            "L": self.L_list[idx1],
+            "evals": self.evals_list[idx1],
+            "evecs": self.evecs_list[idx1],
+            "gradX": self.gradX_list[idx1],
+            "gradY": self.gradY_list[idx1],
+            "name": self.used_shapes[idx1],
+            "sample_idx": self.sample_list[idx1],
+        }
+
+        shape2 = {
+            "xyz": self.verts_list[idx2],
+            "faces": self.faces_list[idx2],
+            "frames": self.frames_list[idx2],
+            "mass": self.massvec_list[idx2],
+            "L": self.L_list[idx2],
+            "evals": self.evals_list[idx2],
+            "evecs": self.evecs_list[idx2],
+            "gradX": self.gradX_list[idx2],
+            "gradY": self.gradY_list[idx2],
+            "name": self.used_shapes[idx2],
+            "sample_idx": self.sample_list[idx2],
+        }
+
+        # Compute fmap
+        map21 = self.corres_dict[(idx1, idx2)]
+
+        evec_1, evec_2, mass2 = shape1["evecs"][:, :self.n_fmap], shape2["evecs"][:, :self.n_fmap], shape2["mass"]
+        trans_evec2 = evec_2.t() @ torch.diag(mass2)
+
+        # If the size of the map vector is too long, shorten it
+        # the removed part only contains 0's and 1's
+        # TODO: check if this should be handled differently and if it still gives a viable map!
+        if map21.size(0) == evec_2.size(0) + evec_1.size(0):
+            map21 = map21[:evec_2.size(0)]
+
+        P = torch.zeros(evec_2.size(0), evec_1.size(0))
+        P[range(evec_2.size(0)), map21.flatten()] = 1
+        C_gt = trans_evec2 @ P @ evec_1
+
+        # compute region labels
+        gt_partiality_mask12 = torch.zeros(shape1["xyz"].size(0)).long().detach()
+        gt_partiality_mask12[map21[map21 != -1]] = 1
+        gt_partiality_mask21 = torch.zeros(shape2["xyz"].size(0)).long().detach()
+        gt_partiality_mask21[map21 != -1] = 1
+
+        return {"shape1": shape1, "shape2": shape2, "C_gt": C_gt,
+                "map21": map21, "gt_partiality_mask12": gt_partiality_mask12, "gt_partiality_mask21": gt_partiality_mask21}
+
+
+def shape_to_device(dict_shape, device):
+    names_to_device = ["xyz", "faces", "mass", "evals", "evecs", "gradX", "gradY"]
+    for k, v in dict_shape.items():
+        if "shape" in k:
+            for name in names_to_device:
+                v[name] = v[name].to(device)
+            dict_shape[k] = v
+        else:
+            dict_shape[k] = v.to(device)
+
+    return dict_shape
--- pyFM/mesh/file_utils.py
+++ pyFM/mesh/file_utils.py
@@ -1,9 +1,11 @@
 import os
 import sys
 from shutil import copyfile
+from collections import defaultdict
+
 import numpy as np
 import pandas as pd
-from collections import defaultdict
+import open3d as o3d
 
 
 def read_off(filepath):
@@ -64,179 +66,12 @@ def read_obj(filepath):
 
     return np.asarray(vertices), np.asarray(faces)
 
-def read_ply(filename, allow_bool=False):
-    """ Read a .ply (binary or ascii) file and store the elements in pandas DataFrame.
-    https://github.com/daavoo/pyntcloud/blob/master/pyntcloud/io/ply.py
-    Parameters
-    ----------
-    filename: str
-        Path to the filename
-    allow_bool: bool
-        flag to allow bool as a valid PLY dtype. False by default to mirror original PLY specification.
-    Returns
-    -------
-    data: dict
-        Elements as pandas DataFrames; comments and ob_info as list of string
-    """
-    sys_byteorder = ('>', '<')[sys.byteorder == 'little']
+def read_ply(path):
+    mesh = o3d.io.read_triangle_mesh(path)
+    # vertices = np.asarray(mesh.vertices)
+    # faces = np.asarray(mesh.triangles, dtype=int)
 
-    ply_dtypes = dict([
-        (b'int8', 'i1'),
-        (b'char', 'i1'),
-        (b'uint8', 'u1'),
-        (b'uchar', 'b1'),
-        (b'uchar', 'u1'),
-        (b'int16', 'i2'),
-        (b'short', 'i2'),
-        (b'uint16', 'u2'),
-        (b'ushort', 'u2'),
-        (b'int32', 'i4'),
-        (b'int', 'i4'),
-        (b'uint32', 'u4'),
-        (b'uint', 'u4'),
-        (b'float32', 'f4'),
-        (b'float', 'f4'),
-        (b'float64', 'f8'),
-        (b'double', 'f8')
-    ])
-
-    valid_formats = {'ascii': '', 'binary_big_endian': '>',
-                 'binary_little_endian': '<'}
-    if allow_bool:
-        ply_dtypes[b'bool'] = '?'
-
-    with open(filename, 'rb') as ply:
-
-        if b'ply' not in ply.readline():
-            raise ValueError('The file does not start with the word ply')
-        # get binary_little/big or ascii
-        fmt = ply.readline().split()[1].decode()
-        # get extension for building the numpy dtypes
-        ext = valid_formats[fmt]
-
-        line = []
-        dtypes = defaultdict(list)
-        count = 2
-        points_size = None
-        mesh_size = None
-        has_texture = False
-        comments = []
-        while b'end_header' not in line and line != b'':
-            line = ply.readline()
-
-            if b'element' in line:
-                line = line.split()
-                name = line[1].decode()
-                size = int(line[2])
-                if name == "vertex":
-                    points_size = size
-                elif name == "face":
-                    mesh_size = size
-
-            elif b'property' in line:
-                line = line.split()
-                # element mesh
-                if b'list' in line:
-
-                    if b"vertex_indices" in line[-1] or b"vertex_index" in line[-1]:
-                        mesh_names = ["n_points", "v1", "v2", "v3"]
-                    else:
-                        has_texture = True
-                        mesh_names = ["n_coords"] + ["v1_u", "v1_v", "v2_u", "v2_v", "v3_u", "v3_v"]
-
-                    if fmt == "ascii":
-                        # the first number has different dtype than the list
-                        dtypes[name].append(
-                            (mesh_names[0], ply_dtypes[line[2]]))
-                        # rest of the numbers have the same dtype
-                        dt = ply_dtypes[line[3]]
-                    else:
-                        # the first number has different dtype than the list
-                        dtypes[name].append(
-                            (mesh_names[0], ext + ply_dtypes[line[2]]))
-                        # rest of the numbers have the same dtype
-                        dt = ext + ply_dtypes[line[3]]
-
-                    for j in range(1, len(mesh_names)):
-                        dtypes[name].append((mesh_names[j], dt))
-                else:
-                    if fmt == "ascii":
-                        dtypes[name].append(
-                            (line[2].decode(), ply_dtypes[line[1]]))
-                    else:
-                        dtypes[name].append(
-                            (line[2].decode(), ext + ply_dtypes[line[1]]))
-
-            elif b'comment' in line:
-                line = line.split(b" ", 1)
-                comment = line[1].decode().rstrip()
-                comments.append(comment)
-
-            count += 1
-
-        # for bin
-        end_header = ply.tell()
-
-    data = {}
-
-    if comments:
-        data["comments"] = comments
-
-    if fmt == 'ascii':
-        top = count
-        bottom = 0 if mesh_size is None else mesh_size
-
-        names = [x[0] for x in dtypes["vertex"]]
-
-        data["points"] = pd.read_csv(filename, sep=" ", header=None, engine="python",
-                                     skiprows=top, skipfooter=bottom, usecols=names, names=names)
-
-        for n, col in enumerate(data["points"].columns):
-            data["points"][col] = data["points"][col].astype(
-                dtypes["vertex"][n][1])
-
-        if mesh_size :
-            top = count + points_size
-
-            names = np.array([x[0] for x in dtypes["face"]])
-            usecols = [1, 2, 3, 5, 6, 7, 8, 9, 10] if has_texture else [1, 2, 3]
-            names = names[usecols]
-
-            data["mesh"] = pd.read_csv(
-                filename, sep=" ", header=None, engine="python", skiprows=top, usecols=usecols, names=names)
-
-            for n, col in enumerate(data["mesh"].columns):
-                data["mesh"][col] = data["mesh"][col].astype(
-                    dtypes["face"][n + 1][1])
-
-    else:
-        with open(filename, 'rb') as ply:
-            ply.seek(end_header)
-            points_np = np.fromfile(ply, dtype=dtypes["vertex"], count=points_size)
-            if ext != sys_byteorder:
-                points_np = points_np.byteswap().newbyteorder()
-            data["points"] = pd.DataFrame(points_np)
-            if mesh_size:
-                mesh_np = np.fromfile(ply, dtype=dtypes["face"], count=mesh_size)
-                if ext != sys_byteorder:
-                    mesh_np = mesh_np.byteswap().newbyteorder()
-                data["mesh"] = pd.DataFrame(mesh_np)
-                data["mesh"].drop('n_points', axis=1, inplace=True)
-
-    return data["points"], data["mesh"]
-
-# def read_ply(ply_file):
-#     with open(ply_file, 'r', encoding = "ISO-8859-1") as f:
-#         lines = f.readlines()
-#         verts_num = int(lines[2].split(' ')[-1])
-#         faces_num = int(lines[6].split(' ')[-1])
-#         verts_lines = lines[9:9 + verts_num]
-#         faces_lines = lines[9 + verts_num:]
-#         verts = np.array([list(map(float, l.strip().split(' '))) for l in verts_lines])
-#         faces = np.array([list(map(int, l.strip().split(' '))) for l in faces_lines])[:,1:]
-
-#     return verts, faces 
-
+    return mesh.vertices, mesh.triangles
 
 def write_off(filepath, vertices, faces, precision=None, face_colors=None):
     """
