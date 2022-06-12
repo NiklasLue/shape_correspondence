69a103d753a30b712a15e9622e7340bb60a85e5a
binlanwu <wubinlan97@gmail.com>
2022-06-12 02:40:38 +0200

    added validation and learning rate scheduler

--- project/train_dpfm.py
+++ project/train_dpfm.py
@@ -5,6 +5,7 @@ import yaml
 import tqdm
 import torch
 
+
 from dpfm.model import DPFMNet
 from dpfm.utils import DPFMLoss, augment_batch
 from project.datasets import ShrecPartialDataset, Tosca, shape_to_device
@@ -29,7 +30,7 @@ def train_net(cfg):
         os.makedirs(os.path.join(base_path, f"data/{save_dir_name}/"))
 
     # create dataset
-    #TODO: implement training / validation split
+    #TODO: implement training / validation split 
     if cfg["dataset"]["name"] == "shrec16":
         train_dataset = ShrecPartialDataset(dataset_path, name=cfg["dataset"]["subset"], k_eig=cfg["fmap"]["k_eig"],
                                             n_fmap=cfg["fmap"]["n_fmap"], use_cache=True, op_cache_dir=op_cache_dir)
@@ -37,7 +38,14 @@ def train_net(cfg):
     elif cfg["dataset"]["name"] == "tosca":
         train_dataset = Tosca(dataset_path, name=cfg["dataset"]["subset"], k_eig=cfg["fmap"]["k_eig"],
                                             n_fmap=cfg["fmap"]["n_fmap"], use_cache=True, op_cache_dir=op_cache_dir, use_adj=True)
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=None, shuffle=True)
+        
+        #TODO set train and validation ratio
+        train, val = torch.utils.data.random_split(train_dataset,[1,1])
+        
+        #Can increase num_workers to speed up training
+        train_loader = torch.utils.data.DataLoader(train, batch_size=None, shuffle=True, num_workers=0)
+        
+        valid_loader = torch.utils.data.DataLoader(val, batch_size=None, shuffle=True, num_workers=0)
     elif cfg["dataset"]["name"] == "faust":
         raise NotImplementedError("FAUST support will come soon!")
     else:
@@ -53,15 +61,17 @@ def train_net(cfg):
     # Training loop
     print("start training")
     iterations = 0
+    lrs = []
+    lambda1 = lambda epoch: 0.65 ** epoch
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda1)
+    
     for epoch in tqdm.tqdm(range(1, cfg["training"]["epochs"] + 1)):
-        # TODO: implement pytorch lr scheduler
-        if epoch % cfg["optimizer"]["decay_iter"] == 0:
-            lr *= cfg["optimizer"]["decay_factor"]
-            print(f"Decaying learning rate, new one: {lr}")
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = lr
+
 
         dpfm_net.train()
+    
+        #training step
+        train_loss = 0.0
         for i, data in enumerate(train_loader):
             data = shape_to_device(data, device)
 
@@ -74,19 +84,56 @@ def train_net(cfg):
             gt_partiality_mask12, gt_partiality_mask21 = data["gt_partiality_mask12"], data["gt_partiality_mask21"]
 
             # do iteration
+            
             C_pred, overlap_score12, overlap_score21, use_feat1, use_feat2 = dpfm_net(data)
-            loss = criterion(C_gt, C_pred, map21, use_feat1, use_feat2,
+            train_loss = criterion(C_gt, C_pred, map21, use_feat1, use_feat2,
                              overlap_score12, overlap_score21, gt_partiality_mask12, gt_partiality_mask21)
-
-            loss.backward()
+            
+            train_loss.backward()
             optimizer.step()
             optimizer.zero_grad()
+            train_loss += train_loss.item()
+        
+        # lr decay
+        scheduler.step()    
+            
+        #validation step  
+        
+        #TODO: early stopping
+        val_loss = 0.0
+        dpfm_net.eval()     # Optional when not using Model Specific layer
+        for i, data in enumerate(valid_loader):
+            data = shape_to_device(data, device)
 
-            # log
+            # data augmentation might consider set different params with the train_data
+            data = augment_batch(data, rot_x=30, rot_y=30, rot_z=60, std=0.01, noise_clip=0.05, scale_min=0.9, scale_max=1.1)
+                
+            # prepare iteration data
+            C_gt = data["C_gt"].unsqueeze(0)
+            map21 = data["map21"]
+            gt_partiality_mask12, gt_partiality_mask21 = data["gt_partiality_mask12"], data["gt_partiality_mask21"]
+            
+            
+            
+            
+            #calculate validation loss after each epoch
+            C_pred, overlap_score12, overlap_score21, use_feat1, use_feat2 = dpfm_net(data)
+            val_loss = criterion(C_gt, C_pred, map21, use_feat1, use_feat2,
+                            overlap_score12, overlap_score21, gt_partiality_mask12, gt_partiality_mask21)
+
+            
+            val_loss += val_loss.item() 
+            
+
+            #log 
             iterations += 1
-            if iterations % cfg["misc"]["log_interval"] == 0:
-                print(f"#epoch:{epoch}, #batch:{i + 1}, #iteration:{iterations}, loss:{loss}")
-
+            #if iterations % cfg["misc"]["log_interval"] == 0:
+            print(f"#epoch:{epoch}, #batch:{i + 1}, #iteration:{iterations}, train_loss:{train_loss}, val_loss:{val_loss}")
+        
+       
+        
+           
+        
         # save model
         if (epoch + 1) % cfg["misc"]["checkpoint_interval"] == 0:
             torch.save(dpfm_net.state_dict(), model_save_path.format(epoch))
