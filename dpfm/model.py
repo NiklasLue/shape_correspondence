7ce349101ee9aa87e2311c5b79ebf6ead05e31d8
binlanwu <wubinlan97@gmail.com>
2022-06-25 20:22:53 +0200

    added DPCFM class

--- dpfm/model.py
+++ dpfm/model.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from typing_extensions import Self
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -201,8 +202,11 @@ class DPFMNet(nn.Module):
                                                         attention_type=cfg["attention"]["attention_type"])
 
         # regularized fmap
-        self.fmreg_net = RegularizedFMNet(lambda_=cfg["fmap"]["lambda_"], resolvant_gamma=cfg["fmap"]["resolvant_gamma"])
+        self.fmreg_net1 = RegularizedFMNet(lambda_=cfg["fmap"]["lambda_"], resolvant_gamma=cfg["fmap"]["resolvant_gamma"])
+
+
         self.n_fmap = cfg["fmap"]["n_fmap"]
+
         self.robust = cfg["fmap"]["robust"]
 
     def forward(self, batch):
@@ -227,6 +231,76 @@ class DPFMNet(nn.Module):
         # predict fmap
         evecs_trans1, evecs_trans2 = evecs1.t()[:self.n_fmap] @ torch.diag(mass1), evecs2.t()[:self.n_fmap] @ torch.diag(mass2)
         evals1, evals2 = evals1[:self.n_fmap], evals2[:self.n_fmap]
-        C_pred = self.fmreg_net(use_feat1, use_feat2, evals1, evals2, evecs_trans1, evecs_trans2)
 
-        return C_pred, overlap_score12, overlap_score21, use_feat1, use_feat2
+        C_pred1 = self.fmreg_net1(use_feat1, use_feat2, evals1, evals2, evecs_trans1, evecs_trans2)
+        C_pred2 = self.fmreg_net1(use_feat2, use_feat1, evals2, evals1, evecs_trans2, evecs_trans1)
+
+
+
+        return C_pred1, C_pred2, overlap_score12, overlap_score21, use_feat1, use_feat2
+
+
+
+
+
+class DPCFMNet(DPFMNet):
+    """Compute the functional map matrix representation."""
+
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+        # feature extractor
+        self.feature_extractor = DiffusionNet(
+            C_in=cfg["fmap"]["C_in"],
+            C_out=cfg["fmap"]["n_feat"],
+            C_width=128,
+            N_block=4,
+            dropout=True,
+            with_gradient_features=True,
+            with_gradient_rotations=True,
+        )
+
+        # cross attention refinement
+        self.feat_refiner = CrossAttentionRefinementNet(n_in=cfg["fmap"]["n_feat"], num_head=cfg["attention"]["num_head"], gnn_dim=cfg["attention"]["gnn_dim"],
+                                                        overlap_feat_dim=cfg["overlap"]["overlap_feat_dim"],
+                                                        n_layers=cfg["attention"]["ref_n_layers"],
+                                                        cross_sampling_ratio=cfg["attention"]["cross_sampling_ratio"],
+                                                        attention_type=cfg["attention"]["attention_type"])
+
+        # regularized fmap
+        self.fmreg_net1 = RegularizedFMNet(lambda_=cfg["fmap"]["lambda_"], resolvant_gamma=cfg["fmap"]["resolvant_gamma"])
+
+
+        self.n_fmap = cfg["fmap"]["n_fmap"]
+
+        self.robust = cfg["fmap"]["robust"]
+
+    def forward(self, batch):
+        verts1, faces1, mass1, L1, evals1, evecs1, gradX1, gradY1 = (batch["shape1"]["xyz"], batch["shape1"]["faces"], batch["shape1"]["mass"],
+                                                                     batch["shape1"]["L"], batch["shape1"]["evals"], batch["shape1"]["evecs"],
+                                                                     batch["shape1"]["gradX"], batch["shape1"]["gradY"])
+        verts2, faces2, mass2, L2, evals2, evecs2, gradX2, gradY2 = (batch["shape2"]["xyz"], batch["shape2"]["faces"], batch["shape2"]["mass"],
+                                                                     batch["shape2"]["L"], batch["shape2"]["evals"], batch["shape2"]["evecs"],
+                                                                     batch["shape2"]["gradX"], batch["shape2"]["gradY"])
+
+        # set features to vertices
+        features1, features2 = verts1, verts2
+
+        feat1 = self.feature_extractor(features1, mass1, L=L1, evals=evals1, evecs=evecs1,
+                                       gradX=gradX1, gradY=gradY1, faces=faces1).unsqueeze(0)
+        feat2 = self.feature_extractor(features2, mass2, L=L2, evals=evals2, evecs=evecs2,
+                                       gradX=gradX2, gradY=gradY2, faces=faces2).unsqueeze(0)
+
+        # refine features
+        ref_feat1, ref_feat2, overlap_score12, overlap_score21 = self.feat_refiner(verts1, verts2, feat1, feat2, batch)
+        use_feat1, use_feat2 = (ref_feat1, ref_feat2) if self.robust else (feat1, feat2)
+        # predict fmap
+        evecs_trans1, evecs_trans2 = evecs1.t()[:self.n_fmap] @ torch.diag(mass1), evecs2.t()[:self.n_fmap] @ torch.diag(mass2)
+        evals1, evals2 = evals1[:self.n_fmap], evals2[:self.n_fmap]
+
+        C_pred1 = self.fmreg_net1(use_feat1, use_feat2, evals1, evals2, evecs_trans1, evecs_trans2)
+        C_pred2 = self.fmreg_net1(use_feat2, use_feat1, evals2, evals1, evecs_trans2, evecs_trans1)
+
+
+
+        return C_pred1, C_pred2, overlap_score12, overlap_score21, use_feat1, use_feat2
