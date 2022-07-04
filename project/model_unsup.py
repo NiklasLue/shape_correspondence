aded5f128ce1291b7601530caaa8264e703953c6
ga27pil <ga27pil@mytum.de>
2022-07-04 17:16:09 +0200

    reorganized code

--- project/model_unsup.py
+++ project/model_unsup.py
@@ -1,54 +1,10 @@
 #from copy import deepcopy
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 from diffusion_net.layers import DiffusionNet
-from dpfm.utils import get_mask #, nn_interpolate
 
-
-
-
-class RegularizedFMNet_unsup(nn.Module):
-    """Compute the functional map matrix representation."""
-
-    def __init__(self, lambda_=1e-3, resolvant_gamma=0.5):
-        super().__init__()
-        self.lambda_ = lambda_
-        self.resolvant_gamma = resolvant_gamma
-
-    def forward(self, feat_x, feat_y, evals_x, evals_y, evecs_trans_x, evecs_trans_y):
-        # compute linear operator matrix representation C1 and C2
-        evecs_trans_x, evecs_trans_y = evecs_trans_x.unsqueeze(0), evecs_trans_y.unsqueeze(0)
-        evals_x, evals_y = evals_x.unsqueeze(0), evals_y.unsqueeze(0)
-
-        F_hat = torch.bmm(evecs_trans_x, feat_x)
-        G_hat = torch.bmm(evecs_trans_y, feat_y)
-        A, B = F_hat, G_hat
-
-        D = get_mask(evals_x.flatten(), evals_y.flatten(), self.resolvant_gamma, feat_x.device).unsqueeze(0)
-        D_t = D.transpose(1, 2)
-
-        A_t = A.transpose(1, 2)
-        A_A_t = torch.bmm(A, A_t)
-        B_A_t = torch.bmm(B, A_t)
-        B_t = B.transpose(1, 2)
-        B_B_t = torch.bmm(B, B_t)
-        A_B_t = torch.bmm(A, B_t)
-
-        C1_i = []
-        C2_i = []
-        for i in range(evals_x.size(1)):
-            D_i = torch.cat([torch.diag(D[bs, i, :].flatten()).unsqueeze(0) for bs in range(evals_x.size(0))], dim=0)
-            C1 = torch.bmm(torch.inverse(A_A_t + self.lambda_ * D_i), B_A_t[:, i, :].unsqueeze(1).transpose(1, 2))
-            C1_i.append(C1.transpose(1, 2))
-            D_t_i = torch.cat([torch.diag(D_t[bs, i, :].flatten()).unsqueeze(0) for bs in range(evals_x.size(0))], dim=0)
-            C2 = torch.bmm(torch.inverse(B_B_t + self.lambda_ * D_t_i), A_B_t[:, i, :].unsqueeze(1).transpose(1, 2))
-            C2_i.append(C2.transpose(1, 2))
-        C1 = torch.cat(C1_i, dim=1)
-        C2 = torch.cat(C2_i, dim=1)
-
-        return C1, C2
-
+from dpfm.model import RegularizedFMNet
 
 class DPFMNet_unsup(nn.Module):
     """Compute the functional map matrix representation."""
@@ -68,7 +24,7 @@ class DPFMNet_unsup(nn.Module):
         )
 
         # regularized fmap
-        self.fmreg_net = RegularizedFMNet_unsup(lambda_=cfg["fmap"]["lambda_"], resolvant_gamma=cfg["fmap"]["resolvant_gamma"])
+        self.fmreg_net = RegularizedFMNet(lambda_=cfg["fmap"]["lambda_"], resolvant_gamma=cfg["fmap"]["resolvant_gamma"])
         self.n_fmap = cfg["fmap"]["n_fmap"]
         # self.robust = cfg["fmap"]["robust"]
 
@@ -91,6 +47,7 @@ class DPFMNet_unsup(nn.Module):
         # predict fmap
         evecs_trans1, evecs_trans2 = evecs1.t()[:self.n_fmap] @ torch.diag(mass1), evecs2.t()[:self.n_fmap] @ torch.diag(mass2)
         evals1, evals2 = evals1[:self.n_fmap], evals2[:self.n_fmap]
-        C1_pred, C2_pred = self.fmreg_net(feat1, feat2, evals1, evals2, evecs_trans1, evecs_trans2)
+        C1_pred = self.fmreg_net(feat1, feat2, evals1, evals2, evecs_trans1, evecs_trans2)
+        C2_pred = self.fmreg_net(feat2, feat1, evals2, evals1, evecs_trans2, evecs_trans1)
 
         return C1_pred, C2_pred, feat1, feat2
