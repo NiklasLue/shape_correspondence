df606699afc793f21191ffb570a442bbdb79bb7e
Niklas Lüdtke <niklas.luedtke@web.de>
2022-07-30 22:26:14 +0200

    Added eval unsup

--- project/eval_dpfm.py
+++ project/eval_dpfm.py
@@ -8,6 +8,7 @@ import tqdm
 
 from project.datasets import ShrecPartialDataset, Tosca, shape_to_device
 from dpfm.model import DPFMNet
+from project.model_unsup import DPFMNet_unsup
 from pyFM.refine import icp
 from pyFM.spectral import FM_to_p2p
 from pyFM.eval.evaluate import accuracy
@@ -146,6 +147,114 @@ def eval_net(cfg, model_path, predictions_name, return_pred_p2p=False, return_di
         return pred_p2p_list
     elif return_dist:
         return distances
+
+def eval_net_unsup(cfg, model_path, predictions_name, return_pred_p2p=False, return_dist=False, mode="FM", n_samples=None):
+    """
+    Rewritten eval_net() function from DPFM
+    """
+    #TODO: tidy up creation of output, depending on the return argumentss
+    #TODO: create option not to calculate any geodesic distances, as this calculation takes the most amount of time
+    print(f"Starting evaluation...")
+    if torch.cuda.is_available() and cfg["misc"]["cuda"]:
+        device = torch.device(f'cuda:{cfg["misc"]["device"]}')
+    else:
+        device = torch.device("cpu")
+    print(f"Using device {device}")
+
+    # important paths
+    base_path = os.path.dirname(__file__)
+    op_cache_dir = cfg["dataset"]["cache_dir"]
+    dataset_path = cfg["dataset"]["root_test"]
+
+    # create dataset
+    print(f"Loading data...")
+    if cfg["dataset"]["name"] == "shrec16":
+        test_dataset = ShrecPartialDataset(dataset_path, name=cfg["dataset"]["subset"], k_eig=cfg["fmap"]["k_eig"],
+                                           n_fmap=cfg["fmap"]["n_fmap"], use_cache=True, op_cache_dir=op_cache_dir)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=None, shuffle=False)
+    elif cfg["dataset"]["name"] == "tosca":
+        # TODO: adjust use_adj, so that it matches with pyFM
+        test_dataset = Tosca(dataset_path, name=cfg["dataset"]["subset"], k_eig=cfg["fmap"]["k_eig"],
+                                           n_fmap=cfg["fmap"]["n_fmap"], use_cache=True, op_cache_dir=op_cache_dir, use_adj=True, n_samples=n_samples)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=None)
+    elif cfg["dataset"]["name"] == "faust":
+        raise NotImplementedError("FAUST support will come soon!")
+    else:
+        raise NotImplementedError("dataset not implemented!")
+
+    # define model
+    print(f"Loading model...")
+    dpfm_net = DPFMNet_unsup(cfg).to(device)
+    dpfm_net.load_state_dict(torch.load(model_path, map_location=device))
+    dpfm_net.eval()
+
+    if mode=="CFM":
+        cfm = CoupledFunctionalMappingDPFM(dpfm_net)
+
+    # initialize logging lists
+    to_save_list = []
+    acc_list = []
+    if return_pred_p2p:
+        pred_p2p_list = []
+    if return_dist:
+        distances = []
+
+    # enumerate through batches of data
+    print(f"Starting inference...")
+    for i, data in enumerate(tqdm.tqdm(test_loader)):
+
+        data = shape_to_device(data, device)
+
+        # prepare iteration data
+        shape1, shape2, p2p_gt = data["shape1"], data["shape2"], data["map21"]
+
+        # do iteration
+        if mode=="FM":
+            p2p_pred, log_obj = FM_batch_eval(data, dpfm_net, shape1, shape2)
+        elif mode=="CFM":
+            C1, C2 = cfm.fit(shape1, shape2, **cfg["cfm_eval"])
+            p2p_pred = cfm.get_p2p_map(n_jobs=-1)
+            log_obj = (data["shape1"]["name"], data["shape2"]["name"], 
+                        C1, C2, data["C_gt"].detach().cpu().unsqueeze(0), data["C2_gt"].detach().cpu().unsqueeze(0),
+                        data["gt_partiality_mask12"].detach().cpu().squeeze(0), data["gt_partiality_mask21"].detach().cpu().squeeze(0),
+                        p2p_pred, data["map21"])
+        else:
+            message = f"mode '{mode}' not supported!"
+            raise ValueError(message)
+
+        if return_pred_p2p:
+            pred_p2p_list.append({'p2p': p2p_pred, 'mesh1': shape1["mesh"], 'mesh2': shape2["mesh"]})
+
+        to_save_list.append(log_obj)
+
+        mesh1_geod = shape1["mesh"].get_geodesic()
+        mesh1_sqrt_area = shape1["mesh"].sqrtarea
+
+        if return_dist:
+
+            mean_dist, dist = accuracy(p2p_pred, p2p_gt.cpu(), mesh1_geod, sqrt_area=mesh1_sqrt_area, return_all=True)
+            distances.extend(dist)
+            acc_list.append(mean_dist)
+        else:    
+            acc_list.append(accuracy(p2p_pred, p2p_gt.cpu(), mesh1_geod, sqrt_area=mesh1_sqrt_area))
+        
+    print(f"Mean normalized geodesic error: {sum(acc_list)/len(acc_list)}")
+    save_obj = {"log_obj": to_save_list, "accuracy": acc_list}
+
+    if return_dist:
+        save_obj["distances"] = distances
+    if return_pred_p2p:
+        save_obj["pred_p2p"] = pred_p2p_list
+    
+    torch.save(save_obj, predictions_name)
+
+    if return_pred_p2p and return_dist:
+        return pred_p2p_list, distances
+    elif return_pred_p2p:
+        return pred_p2p_list
+    elif return_dist:
+        return distances
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Launch the training of DPFM model.")
